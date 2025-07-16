b862579919a8a4aa40de56014c1bdcf611567237
Diego <70644302+NrtypmForhp@users.noreply.github.com>
2025-07-16 20:26:09 +0200

    Add files via upload

--- static/goods.js
+++ static/goods.js
@@ -1,11 +1,22 @@
 // [= TORCH goods javascript file =]
 
+// Function to clear item management window
+function clear_item_management_window(){
+    // Inputs
+    $("#item-management-category-input").val("");
+    $("#item-management-type-input").val("");
+
+    // Details p
+    $("#item-management-details-p").text("");
+}
+
 $(document).ready(function(){
     // Click on new item button
-    $(document).on("click", "#new_item_button", function(){ // Open new item div
-        $("#div-new-item").css("display", "block");
+    $(document).on("click", "#new-item-button", function(){ // Open new item div
+        $("#div-item-management").css("display", "block");
+        clear_item_management_window();
     });
-    $(document).on("click", "#new-item-close", function(){ // Close new item div
-        $("#div-new-item").css("display", "none");
+    $(document).on("click", "#item-management-close", function(){ // Close new item div
+        $("#div-item-management").css("display", "none");
     });
 });
